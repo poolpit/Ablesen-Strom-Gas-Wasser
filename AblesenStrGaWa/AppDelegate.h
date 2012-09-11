e54ef126972cc4eea35cb2339bb8527036d33a90
Peter Wiest <peter@peterpan.fritz.box>
2012-09-11 21:55:08 +0200

    Initial Commit

---
+++ AblesenStrGaWa/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AblesenStrGaWa
+//
+//  Created by Peter Wiest on 11.09.12.
+//  Copyright (c) 2012 Peter Wiest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AblesenStrGaWa/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AblesenStrGaWa
+//
+//  Created by Peter Wiest on 11.09.12.
+//  Copyright (c) 2012 Peter Wiest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
